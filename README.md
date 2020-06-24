d476a932967fe122ee8cdaa89edf1e500fe3632f
Naiara Abaroa <naiara.abaroa@transferwise.com>
2020-06-24 09:51:35 +0100

    Build initial docs and patterns

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # grid-layout
+
+⚠️ This is an experiment
+
+## Objetive: 
+
+* Take advantage of new grid layout features to apply it in our products.
+* Build an stable layout system, with nice fallback for unsupported browsers, to create the column layout and some reusable patterns.
+* Replace the Bootstrap cols system, instead of using both.
+* Document the process of learnings.
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require("gulp");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const postcssPresetEnv = require("postcss-preset-env");
+const cssImport = require("postcss-import");
+const nested = require("postcss-nested");
+const cssnano = require("cssnano");
+const notify = require("gulp-notify");
+const root = require("postcss-remove-root");
+
+function css() {
+  return gulp
+    .src("./src/less/grid-layout.less")
+    .pipe(less())
+    .pipe(
+      postcss([
+        cssImport(),
+        nested(),
+        postcssPresetEnv({
+          //   autoprefixer: { grid: true },
+          stage: 1,
+        }),
+        root(),
+      ])
+    )
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest("./css/"))
+    .pipe(
+      notify({
+        message: "Your CSS is ready ♡",
+      })
+    );
+}
+
+function neptune() {
+  return gulp
+    .src("./src/less/neptune.less")
+    .pipe(less())
+    .pipe(postcss([cssImport()]))
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest("./css/"))
+    .pipe(
+      notify({
+        message: "Your Neptune CSS is ready ♡",
+      })
+    );
+}
+
+function watch() {
+  gulp.watch("./src/*.less");
+  //   gulp.watch("./src/neptune.less", neptune);
+}
+
+const build = gulp.series(css, neptune);
+
+exports.css = css;
+exports.neptune = neptune;
+exports.watch = watch;
+exports.default = build;
